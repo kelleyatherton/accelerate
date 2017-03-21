71f3150edb11efdd6b1018e81f79c9bb8b5ade29
kelleyatherton <kelleyatherton@gmail.com>
2017-03-21 17:27:06 -0500

    create child theme

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -29,7 +29,6 @@ get_header(); ?>
 	     <div class="blog-post">
 	     <h4>From the Blog</h4>
 			 <?php query_posts('posts_per_page=1'); ?>
-			 <?php query_posts('posts_per_page=1'); ?>
   <?php while ( have_posts() ) : the_post(); ?>
 		<h2><?php the_title(); ?></h2>
        <?php the_excerpt(); ?>
